d5d67b40bb5138501948e121534a36042424325d
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-25 16:20:37 +0300

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -4,11 +4,18 @@ import Filter from "./Filter";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [queryInput, setQueryInput] = useState("")
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
+  
+  function handleInputChange(e){
+    setQueryInput(e.target.value.toLocaleUpperCase())
+  }
+
+  console.log(queryInput)
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
@@ -19,9 +26,9 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleInputChange}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {itemsToDisplay.filter((query) => query.name.toUpperCase().includes(queryInput)).map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
